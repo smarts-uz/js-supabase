be7e61530160662d820b1424c53e289eb94db5c7
Elchinbek Jumanazarov <elchinbekjumanazarov@gmail.com>
2024-05-07 09:45:45 +0500

    create client With additional parameters

--- Initializing/With additional parameters.js
+++ Initializing/With additional parameters.js
@@ -14,4 +14,5 @@ const options = {
     headers: { 'x-my-custom-header': 'my-app-name' },
   },
 }
-const supabase = createClient("https://xyzcompany.supabase.co", "public-anon-key", options)
+const supabase = createClient("https://torthiwfsqwzbcveiimx.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvcnRoaXdmc3F3emJjdmVpaW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU5MDkxODUsImV4cCI6MjAxMTQ4NTE4NX0.9FnyfQPJQ5puR_MQnK7RvwiBeuYy7DNjBZcVH1y3eOI", options)
+console.log(supabase)
