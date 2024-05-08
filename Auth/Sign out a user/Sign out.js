cc0c53f817388339673769d9961908a9493ae23e
Elchinbek Jumanazarov <elchinbekjumanazarov@gmail.com>
2024-05-08 16:46:57 +0500

    auth

---
+++ Auth/Sign out a user/Sign out.js
@@ -0,0 +1,19 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient('https://okobszhsjvdkgoimnjwa.supabase.co', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rb2JzemhzanZka2dvaW1uandhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwMjc2MDUsImV4cCI6MjAyOTYwMzYwNX0._rvvKrnKchELmrDvHyRc062cIhQJirmQxWYBDDReBWA")
+
+
+const { data, error } = await supabase.auth.signInWithPassword({
+    email: 'bikeeee@gmail.com',
+    password: 'passs321321daseqw12354421',
+})
+console.log(data)
+console.log(error)
+
+
+
+//
+await supabase.auth.signOut()
+
+console.log('error')
