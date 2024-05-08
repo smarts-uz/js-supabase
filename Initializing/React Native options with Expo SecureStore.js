e5355b1053b4d4c1563b02bc6b5f197d81afe926
Elchinbek Jumanazarov <elchinbekjumanazarov@gmail.com>
2024-05-08 13:25:11 +0500

    some changes

--- Initializing/React Native options with Expo SecureStore.js
+++ Initializing/React Native options with Expo SecureStore.js
@@ -3,7 +3,7 @@ import 'react-native-url-polyfill/auto'
 import { createClient } from '@supabase/supabase-js'
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const supabase = createClient("https://xyzcompany.supabase.co", "public-anon-key", {
+const supabase = createClient("https://okobszhsjvdkgoimnjwa.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rb2JzemhzanZka2dvaW1uandhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQwMjc2MDUsImV4cCI6MjAyOTYwMzYwNX0._rvvKrnKchELmrDvHyRc062cIhQJirmQxWYBDDReBWA", {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
