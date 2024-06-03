9dfd52675a0e21b6ccc165e16e6010749077ac3e
Elchinbek Jumanazarov <elchinbekjumanazarov@gmail.com>
2024-06-03 19:53:24 +0500

    sign in & sign up

--- Auth/Sign in a user/Sign in with email and password.js
+++ Auth/Sign in a user/Sign in with email and password.js
@@ -1,12 +1,13 @@
 
 import { createClient } from '@supabase/supabase-js'
-
+import 'dotenv/config'
 const supabase = createClient('https://torthiwfsqwzbcveiimx.supabase.co', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvcnRoaXdmc3F3emJjdmVpaW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU5MDkxODUsImV4cCI6MjAxMTQ4NTE4NX0.9FnyfQPJQ5puR_MQnK7RvwiBeuYy7DNjBZcVH1y3eOI")
 
 
 const { data, error } = await supabase.auth.signInWithPassword({
   email: 'bikeeee@gmail.com',
-  password: 'passs321321daseqw12354421',
+  // password: 'passs321321daseqw12354421',
+  password: 'passs321312321daseqw12354421',
 })
 console.log(data)
 console.log(error)
